ab1252842f379c1fc2d41c77720bb59bdb39194e
Md. Ibna Siam <siamibna75@gmail.com>
2026-08-02 01:45:46 +0600

    SpeedInsights

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
@@ -9,6 +10,11 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Loader from "./components/Loader";
 import { useTheme } from "./hooks";
+import { SpeedInsights } from '@vercel/speed-insights/react';
+
+
+
+
 
 export default function App() {
   const [dark, setDark] = useTheme();
